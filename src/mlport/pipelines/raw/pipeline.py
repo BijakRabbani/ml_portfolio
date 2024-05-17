39a4d836671f0fcb081d9a6beb97992dd138ff50
Bijak Rabbani <rabbani.bijak@gmail.com>
2024-05-17 07:52:36 +0700

    Add raw_market_data

--- src/mlport/pipelines/raw/pipeline.py
+++ src/mlport/pipelines/raw/pipeline.py
@@ -1,5 +1,4 @@
 from kedro.pipeline import Pipeline, node
-from functools import partial, update_wrapper
 from .nodes_ksei import (
     get_stock_info, 
     update_stock_listed,
@@ -7,15 +6,11 @@ from .nodes_ksei import (
 from .nodes_market import (
     update_date_list,
     update_index_values,
-#     get_market_data,
-#     select,
-#     calculate_daily_return,
+    get_market_data,
     )
 
 
 def create_pipeline(**kwargs):
-    # select_adj_close = partial(select, column='adj_close')
-    # update_wrapper(select_adj_close, select)
     return Pipeline(
         [
             node(
@@ -42,35 +37,11 @@ def create_pipeline(**kwargs):
                 "stock_listed",
                 name="stock_listed",
             ),
-            # node(
-            #     get_market_data,
-            #     'stock_listed',
-            #     "raw_market_data",
-            #     name="raw_market_data",
-            # ),
-            # node(
-            #     select_adj_close, 
-            #     'raw_market_data',
-            #     "adj_close",
-            #     name="adj_close",
-            # ),
-            # node(
-            #     calculate_daily_return, 
-            #     'adj_close',
-            #     "daily_return",
-            #     name="daily_return",
-            # ),
-            # node(
-            #     get_intraday_data,
-            #     ['date_list', 'listed', 'delisted'],
-            #     ['intraday_stock', 'intraday_jci'],
-            #     name='intraday'
-            # ),
-            # node(
-            #     get_ca_data,
-            #     None,
-            #     'corporate_action_raw',
-            #     name='corporate_action_raw'
-            # )
+            node(
+                get_market_data,
+                'stock_listed',
+                "raw_market_data",
+                name="raw_market_data",
+            ),
         ]
     )
